2490dfa817aa0ec81aa5b7fe91383abdbdd63bee
Lukas Dorn-Fussenegger <dornfussenegger@battain.at>
2020-02-11 10:44:40 +0100

    updated tests

---
+++ ILGenerator.Test.NetCoreTests/DefinitionTests.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using YamlDotNet.Serialization;
+
+namespace ILGenerator.Test.NetCoreTests
+{
+
+    [TestClass]
+    public class DefinitionTests
+    {
+
+        public void GeneralTest()
+        {
+            var b = new ClassDefinition.BuildContextDefinition();
+
+            var links = b.AddType("Url", ClassDefinition.DefinitionTypeEnums.SimpleType);
+            links.AddProperty("Url", typeof(string));
+            links.AddProperty("Title", typeof(string));
+
+            var address = b.AddType("Address", ClassDefinition.DefinitionTypeEnums.SimpleType);
+            address.AddProperty("Street", typeof(string));
+            address.AddProperty("Housenumber", typeof(string));
+            address.AddProperty("PostcalCode", typeof(string));
+            address.AddProperty("Country", typeof(string));
+
+            var test = b.AddType("Test", ClassDefinition.DefinitionTypeEnums.SimpleType);
+            test.AddProperty("FirstName", typeof(string));
+            test.AddProperty("LastName", typeof(string));
+            test.AddProperty("Uid", typeof(Guid));
+            test.AddProperty("Address", address);
+            test.AddPropertyOfList("Links", links);
+
+            var defJson = Newtonsoft.Json.JsonConvert.SerializeObject(b, Newtonsoft.Json.Formatting.Indented);
+            {
+                var t = b.Compile("test").FirstOrDefault(w => w.Name == "Test");
+                var i = t.CreateInstance() as Interfaces.IPropertyGetAndSet;
+                var linkCollection = i.GetPropertyValue<BaseClasses.IGenericAddNewList>("Links");
+
+                int x = 0;
+                foreach (var item in linkCollection.CreateAndAdd(3))
+                {
+                    x += 1;
+                    var l = item as Interfaces.IPropertyGetAndSet;
+                    l.SetPropertyValue("Url", $"{x}");
+                    l.SetPropertyValue("Title", $"{x}");
+                }
+
+                var serializer = new SerializerBuilder().Build();
+
+                var yaml = serializer.Serialize(b);
+
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(i, Newtonsoft.Json.Formatting.Indented);
+
+                Console.WriteLine(yaml);
+                Console.WriteLine(
+                    json
+                );
+            }
+
+
+
+
+            {
+                var deserialized = Newtonsoft.Json.JsonConvert.DeserializeObject<ClassDefinition.BuildContextDefinition>(defJson);
+                var b2 = deserialized;
+                var t = b2.Compile("test").FirstOrDefault(w => w.Name == "Test");
+                var i = t.CreateInstance() as Interfaces.IPropertyGetAndSet;
+                var linkCollection = i.GetPropertyValue<BaseClasses.IGenericAddNewList>("Links");
+
+                int x = 0;
+                foreach (var item in linkCollection.CreateAndAdd(3))
+                {
+                    x += 1;
+                    var l = item as Interfaces.IPropertyGetAndSet;
+                    l.SetPropertyValue("Url", $"{x}");
+                    l.SetPropertyValue("Title", $"{x}");
+                }
+
+                var serializer = new SerializerBuilder().Build();
+
+                var yaml = serializer.Serialize(b2);
+
+                var json = Newtonsoft.Json.JsonConvert.SerializeObject(i, Newtonsoft.Json.Formatting.Indented);
+
+                Console.WriteLine(yaml);
+                Console.WriteLine(
+                    json
+                );
+            }
+        }
+
+    }
+}
--- ILGenerator.Test.NetCoreTests/ILGeneratorTests.cs
+++ ILGenerator.Test.NetCoreTests/ILGeneratorTests.cs
@@ -9,6 +9,31 @@ namespace ILGenerator.Test.NetCoreTests
     {
 
         [TestMethod]
+        public void GeneralTest()
+        {
+            var b = new ClassDefinition.BuildContextDefinition();
+            var test = b.AddType("Test", ClassDefinition.DefinitionTypeEnums.SimpleType);
+            test.AddProperty("FirstName", typeof(string));
+            test.AddProperty("LastName", typeof(string));
+            test.AddProperty("Uid", typeof(Guid));
+
+            var t = b.Compile("TestAssembly")[0];
+            dynamic instance = t.CreateInstance<dynamic>();
+            instance.FirstName = "Lukas";
+            instance.LastName = "Dorn-Fussenegger";
+            instance.Uid = Guid.NewGuid();
+
+            var json = Newtonsoft.Json.JsonConvert.SerializeObject(instance);
+            Console.WriteLine(json);
+
+            dynamic deserialized = Newtonsoft.Json.JsonConvert.DeserializeObject(json, t);
+            //Creates: {"FirstName":"Lukas","LastName":"Dorn-Fussenegger","Uid":"7183685b-b314-4fd8-8bc5-7488e52618c4"}
+
+            Assert.IsTrue(deserialized.FirstName == "Lukas");
+
+        }
+
+        [TestMethod]
         public void GetPropertyNamesAndTypes()
         {
             CustomTypeCreator ct = new CustomTypeCreator("testassembly");
